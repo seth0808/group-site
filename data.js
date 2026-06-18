cf7a8132c818cb20338505e4a5d8f1c39ca73457
seth <sethkujawa@gmail.com>
2026-06-17 22:18:12 -0500

    WV Submissions

--- data.js
+++ data.js
@@ -8,6 +8,13 @@ const PROMPTS = [
     slug: "west-virginia",
     title: "West Virginia",
     date: "2026-05-31",
-    submissions: []
+    submissions: [
+      { title: "Naked", author: "Atharva", file: "submissions-wv/atharva/index.html" },
+      { title: "Playlist", author: "David P.", file: "submissions-wv/david-p/index.html" },
+      { title: "Trip Report", author: "David R.", file: "submissions-wv/david-r/DAVID_TRIP_REPORT_2026.html" },
+      { title: "Masterpiece", author: "Quinn", file: "submissions-wv/quinn/ascii_art.html" },
+      { title: "Time Traveler", author: "Ransom", file: "submissions-wv/ransom/time_traveler.html" },
+      { title: "The Fish Will Be Made Whole Again", author: "Seth", file: "submissions-wv/seth/story.html" }
+    ]
   }
 ];
